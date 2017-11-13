23c4d69af6436469bfe0ab2c27257c7ae059dadb
alexd448 <alexanderdagilis@muhlenberg.edu>
2017-11-13 12:45:30 -0500

    Coded declareWin and checkWin

--- UTTT/Board.java
+++ UTTT/Board.java
@@ -8,6 +8,7 @@ import java.util.*;
 public class UTTT
 {
   private static char[] boardArr = new char[9];
+  private static int WIndex = -1;
 
   public Board(){
       buildBoard();
@@ -46,16 +47,67 @@ public int[] getBoard(){
     }
   }
 
-  public void checkWin(){
-    return;
+  public boolean checkWin(){
+    if (boardArr[0] == boardArr[1] && boardArr[0] == boardArr[2] && boardArr[0] != ' ')
+    {
+      WIndex = 0;
+      return true;
+    }
+    else if (boardArr[3] == boardArr[4] && boardArr[3] == boardArr[5] && boardArr[3] != ' ')
+    {
+      WIndex = 3;
+      return true;
+    }
+    else if (boardArr[6] == boardArr[7] && boardArr[6] == boardArr[8] && boardArr[6] != ' ')
+    {
+      WIndex = 6;
+      return true;
+    }
+    else if (boardArr[0] == boardArr[3] && boardArr[0] == boardArr[6] && boardArr[0] != ' ')
+    {
+      WIndex = 0;
+      return true;
+    }
+    else if (boardArr[1] == boardArr[4] && boardArr[1] == boardArr[7] && boardArr[1] != ' ')
+    {
+      WIndex = 1;
+      return true;
+    }
+    else if (boardArr[2] == boardArr[5] && boardArr[2] == boardArr[8] && boardArr[2] != ' ')
+    {
+      WIndex = 2;
+      return true;
+    }
+    else if (boardArr[0] == boardArr[4] && boardArr[0] == boardArr[8] && boardArr[0] != ' ')
+    {
+      WIndex = 0;
+      return true;
+    }
+    else if (boardArr[2] == boardArr[4] && boardArr[2] == boardArr[6] && boardArr[2] != ' ')
+    {
+      WIndex = 2;
+      return true;
+    }
+    else
+        return false;
   }
 
-  public void declareWin(){
-    return;
+
+  public char declareWin(){
+    if (checkWin == true){
+      if (boardArr[wIndex] == 'O'){
+        return 'O';
+      }
+      else {
+        return 'X';
+      }
+    }
+    else
+      return 'T';
   }
 
   public void checkMove(int moveLoc){
-    if (boardArr[moveLoc] == " ")
+    if (boardArr[moveLoc] == ' ')
       placeMove(moveLoc);
     else
       placeMove(-1);
@@ -64,7 +116,7 @@ public int[] getBoard(){
   public void placeMove(int moveLoc){
 
    if (moveLoc >= 0 && moveLoc <= 8){
-      boardArr[moveLoc] = "O";
+      boardArr[moveLoc] = 'O';
       System.out.println("Move Made");
     }
     else{
