1904735868935abc44f76151ee2c93273aa698ed
ko251030 <ko251030@muhlenberg.edu>
2017-11-29 12:35:53 -0500

    i did some work on the bord. Still not right tho

--- UTTT/TicGUI.java
+++ UTTT/TicGUI.java
@@ -10,7 +10,7 @@ public class TicGUI extends JFrame
 JButton[][] buttons = new JButton[9][9];
 JFrame frame = new JFrame("TicTacToe");                    //Global frame and grid button variables
 JButton reset = new JButton("Reset");             //Create reset button for game
-//JOptionPane turn;
+JOptionPane turn;
 int moveCounter = 9;
 boolean gameWon = false;
 int WhoseTurn = 1;
@@ -18,10 +18,18 @@ int WhoseTurn = 1;
 public TicGUI()                                        //Tic tac default constructor which adds and dimensions Jframe
 {
  super();
- frame.setSize(1000, 1000);
- frame.setDefaultCloseOperation(EXIT_ON_CLOSE);        //Setting dimension of Jframe and setting parameters
- frame.setVisible(true);
- frame.setResizable(true);
+ //frame.setSize(1000, 1000);
+ //frame.setDefaultCloseOperation(EXIT_ON_CLOSE);        //Setting dimension of Jframe and setting parameters
+ //frame.setVisible(true);
+ //frame.setResizable(true);
+
+ JButton[][] buttons = new JButton[9][9];
+ int moveCounter = 9;
+ boolean gameWon = false;
+
+ //int WhoseTurn = 1;
+
+
 }
 
 private void checkWin(int row, int col)
@@ -97,6 +105,7 @@ int randomMove=count;
 Random num = new Random();
 randomMove = num.nextInt(randomMove)+1;
 
+
 }
 
 private void initialize()             //Initialize tic tac toe game board
@@ -106,7 +115,7 @@ private void initialize()             //Initialize tic tac toe game board
   JPanel game = new JPanel(new GridLayout(9,3));
   //JPanel game2 = new JPanel(new GridLayout(3,3));                    //Create two more panels with layouts for buttons
 
-  frame.add(mainPanel);                                         //add main container panel to frame
+  //frame.add(mainPanel);                                         //add main container panel to frame
 
   //mainPanel.setPreferredSize(new Dimension(325,425));
   menu.setPreferredSize(new Dimension(300,50));                     //Setting dimensions of panels
@@ -118,9 +127,9 @@ private void initialize()             //Initialize tic tac toe game board
   //mainPanel.add(game2,BorderLayout.CENTER);
 
   //Add both start/reset buttons to menu container panel
-  menu.add(reset, BorderLayout.NORTH);
+  //menu.add(reset, BorderLayout.NORTH);
 
-  reset.addActionListener(new myActionListener());
+  //reset.addActionListener(new myActionListener());
 
 for(int i = 0; i < 9; i++)                      //Create grid of buttons for tic tac toe game
  {
@@ -227,27 +236,125 @@ private class myActionListener implements ActionListener
      checkWin(2,0);
     }
    }
-   if(a.getSource() == reset)
-     {
-        for(int i = 0; i < 3; i++)
-         {
-           for(int j = 0; j < 3; j++)
-            {
-                gameWon = false;
-               buttons[i][j].setText("");
-               buttons[i][j].setEnabled(true);
-               moveCounter = 9;
-               WhoseTurn = 1;
+   //if(a.getSource() == reset)
+     //{
+      //  for(int i = 0; i < 3; i++)
+        // {
+          // for(int j = 0; j < 3; j++)
+            //{
+              //  gameWon = false;
+               //buttons[i][j].setText("");
+               //buttons[i][j].setEnabled(true);
+               //moveCounter = 9;
+               //WhoseTurn = 1;
 
-            }
-         }
-       }
+            //}
+         //}
+       //}
   }
  }
 
 public static void main(String[] args)
 {
   TicGUI game = new TicGUI();         //main method and instantiating tic tac object and calling initialize function
-  game.initialize();
+  //game.initialize();
+  game.makeGame();
+
  }
+
+public void makeGame()
+{
+  TicGUI[] tac = new TicGUI[9];
+
+  TicGUI game1 = new TicGUI();
+  TicGUI game2 = new TicGUI();
+  TicGUI game3 = new TicGUI();
+  TicGUI game4 = new TicGUI();
+  TicGUI game5= new TicGUI();
+  TicGUI game6 = new TicGUI();
+  TicGUI game7 = new TicGUI();
+  TicGUI game8 = new TicGUI();
+  TicGUI game9 = new TicGUI();
+
+  tac[0] = game1;
+  tac[1] = game2;
+  tac[2] = game3;
+  tac[3] = game4;
+  tac[4] = game5;
+  tac[5] = game6;
+  tac[6] = game7;
+  tac[7] = game8;
+  tac[8] = game9;
+
+  frame.setSize(1000, 1000);
+  frame.setDefaultCloseOperation(EXIT_ON_CLOSE);        //Setting dimension of Jframe and setting parameters
+  frame.setVisible(true);
+  frame.setResizable(true);
+  JPanel mainPanel = new JPanel(new BorderLayout());         //create main panel container to put layer others on top
+  JPanel menu = new JPanel(new BorderLayout());
+  JPanel gameDisplay = new JPanel(new GridLayout(3,3));
+  gameDisplay.add(tac[0]);
+
+
+  frame.add(mainPanel);                                         //add main container panel to frame
+
+  //mainPanel.setPreferredSize(new Dimension(325,425));
+  menu.setPreferredSize(new Dimension(1000,50));                     //Setting dimensions of panels
+  gameDisplay.setPreferredSize(new Dimension(900,900));
+
+  mainPanel.add(menu, BorderLayout.NORTH);                   //Add two panels to the main container panel
+  mainPanel.add(gameDisplay, BorderLayout.SOUTH);
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+public void makeBord()
+{
+  frame.setSize(1000, 1000);
+  frame.setDefaultCloseOperation(EXIT_ON_CLOSE);        //Setting dimension of Jframe and setting parameters
+  frame.setVisible(true);
+  frame.setResizable(true);
+  JPanel mainPanel = new JPanel(new BorderLayout());         //create main panel container to put layer others on top
+  JPanel menu = new JPanel(new BorderLayout());
+  JPanel game = new JPanel(new GridLayout(3,3));
+
+
+  //frame.add(mainPanel);                                         //add main container panel to frame
+
+  //mainPanel.setPreferredSize(new Dimension(325,425));
+  menu.setPreferredSize(new Dimension(300,50));                     //Setting dimensions of panels
+  game.setPreferredSize(new Dimension(900,900));
+  //game2.setPreferr.edSize(new Dimension(300,300));
+
+  mainPanel.add(menu, BorderLayout.NORTH);                   //Add two panels to the main container panel
+  mainPanel.add(game, BorderLayout.SOUTH);
+  //mainPanel.add(game2,BorderLayout.CENTER);
+}
+
+
+
+
+
 }
