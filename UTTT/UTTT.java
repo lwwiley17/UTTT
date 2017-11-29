49c412e3c7ac7380653b8daceca3512b35c1e9c6
alexd448 <alexanderdagilis@muhlenberg.edu>
2017-11-29 10:16:40 -0500

    Test of PlayGame

--- UTTT/UTTT.java
+++ UTTT/UTTT.java
@@ -10,11 +10,7 @@ public class UTTT
 
   public static void main(String[] args)
   {
-<<<<<<< HEAD
-    
-=======
       PlayGame game1 = new PlayGame("basic");
->>>>>>> 6ad9a84bbdbe788ed13dfc9abf902170d04585b2
   }
 
 }
